89bcd682651723dcc7b2f2049996d6217cfcdb3a
Uche O. Emmanuel <ucheodilachi@gmail.com>
2024-02-01 00:28:07 +0100

    modified

--- Day-2/My-Portfolio/src/App.jsx
+++ Day-2/My-Portfolio/src/App.jsx
@@ -1,11 +1,18 @@
-import NavBar from "./component/NavBar"
-
+import NavBar from "./component/navBar/NavBar"
+import Intro from "./component/Intro/intro"
+import About from "./component/About/about";
+import Portfolio from "./component/Portfolio/portfolio";
+import Contact from "./component/contact/contact";
 const App = () => {
   return (
     <div>
       <NavBar />
+      <Intro />
+      <About />
+      <Portfolio />
+      <Contact />
     </div>
-  )
+  );
 }
 
 export default App
