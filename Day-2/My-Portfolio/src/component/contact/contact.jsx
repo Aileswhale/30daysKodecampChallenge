70318860cb9b9ecee86bc715d5f379e0af47b907
Uche O. Emmanuel <ucheodilachi@gmail.com>
2024-02-02 17:24:13 +0100

    installed react-icons and modified the contact page

--- Day-2/My-Portfolio/src/component/contact/contact.jsx
+++ Day-2/My-Portfolio/src/component/contact/contact.jsx
@@ -1,36 +1,50 @@
-import React from 'react'
+import {FaFacebook, FaInstagram, FaLinkedin, FaTwitter, FaWhatsapp, FaYoutube } from "react-icons/fa";
 
 const Contact = () => {
   return (
     <>
       <section className="client"></section>
 
-      <section className="contact max-w-[95%] min-h-[90vh] w-[100vw] flex flex-col items-center justify-center mx-[auto] my-0 text-center px-[1rem]">
-        <h1> Contact Me</h1>
-        <p>Please fill the form below to discusss any work opportunity</p>
-        <form className="flex flex-col" action="">
-          <input type="text" placeholder="Your name" className="border" />
-          <input type="email" placeholder="Your email" className="border" />
+      <section className="contact max-w-[95%] min-h-[90vh] w-[100vw] mx-[auto] my-0 text-center px-[1rem]">
+        <h1 className="font-[600] text-[2rem] my-[1.5rem] "> Contact Me</h1>
+        <p className="font-[200] mb-[1.2rem]">
+          Please fill the form below to discusss any work opportunity
+        </p>
+        <form className="flex flex-col w-[70%] m-[auto] " action="">
+          <input
+            type="text"
+            placeholder="Your name"
+            className="rounded-[0.7rem] my-[0.5rem] bg-[#282828] py-[0.5rem] px-[1rem] outline-none text-white"
+          />
+          <input
+            type="email"
+            placeholder="Your email"
+            className="rounded-[0.7rem] my-[0.5rem] bg-[#282828] py-[0.5rem] px-[1rem] outline-none text-white "
+          />
           <textarea
             name="message"
             rows="5"
             placeholder="Your message"
-            className="border"
+            className=" rounded-[0.7rem] my-[0.5rem] bg-[#282828] py-[0.5rem] px-[1rem] outline-none text-white "
           ></textarea>
-          <button type="submit" value="send">
-            Submit
-          </button>
-          {/* <div className='links'>
-                      <img src={k } alt="icons" className='links' />
-                      <img src={k } alt="icons" className='links' />
-                      <img src={m } alt="icons" className='links' />
-                      <img src={ jk} alt="icons" className='links' />
-                      
-                  </div> */}
         </form>
+        <button
+          type="submit"
+          value="send"
+          className="bg-white rounded-[0.5rem] m-[2rem] py-[0.75rem] px-[3.8rem] text-black"
+        >
+          Submit
+        </button>
+        <div className="flex  justify-center items-center gap-[1rem] pt-[2rem">
+            <FaFacebook className="text-[2.5rem]  text-[#847b64]"/>
+          <FaInstagram className="text-[2.5rem]  text-[#847b64]  " />
+          <FaWhatsapp className="text-[2.5rem]  text-[#847b64]  " />
+          <FaLinkedin className="text-[2.5rem]  text-[#847b64]" />
+          <FaTwitter className="text-[2.5rem]  text-[#847b64]  " />
+        </div>
       </section>
     </>
   );
-}
+};
 
-export default Contact
+export default Contact;
